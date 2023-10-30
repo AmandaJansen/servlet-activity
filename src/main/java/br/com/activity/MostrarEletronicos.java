c30ad1477f58980a675a0c4eaba029f08734a89e
Amanda Gracielly Martins Jansen <amanda.jansen@ibm.com>
2023-10-30 19:30:35 -0300

    servlet-activity-up

--- src/main/java/br/com/activity/MostrarEletronicos.java
+++ src/main/java/br/com/activity/MostrarEletronicos.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
+@WebServlet(name = "Eletros", urlPatterns = {"/eletronico"})
 public class MostrarEletronicos extends HttpServlet {
 
     @Override
